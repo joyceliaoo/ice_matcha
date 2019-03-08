62db623b1384151750b232855b6e682bf42fcce4
Joyce Liao <jycqliao@gmail.com>
2019-03-08 00:02:43 -0500

    finishing k8

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -1,11 +1,11 @@
-#Team -- Joyce Liao, Aaron Li, Joan Chirinos
+#Team Mongo-lians -- Joyce Liao, Aaron Li, Joan Chirinos
 #SoftDev1 pd8
 #K08 -- Ay Mon, Go Git It From Yer Flask
 #2019-03-08
 
 import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 from util import mongo
 
@@ -17,16 +17,21 @@ app.secret_key = os.urandom(32)
 def home():
     return render_template("nobel.html")
 
-@app.route('/search')
+@app.route('/search', methods=["POST"])
 def getData():
-    type = request.args["type"]
-    data = mongo.getData(request.args["arg"], type)
+    type = request.form.get("type")
+    data = mongo.getData(request.form.get("arg").lower(), type)
+    if (data==[]):
+        flash("No results found")
     return render_template("result.html", data=data)
 
-@app.route("/launch")
+@app.route("/launch", methods=["POST"])
 def launch():
-    addr = request.args["ip"]
-    mongo.launchDB(addr)
+    try:
+        addr = request.form.get("ip")
+        mongo.launchDB(addr)
+    except:
+        flash("Invalid server address")
     return redirect(url_for("home"))
 
 if (__name__) == ("__main__"):
